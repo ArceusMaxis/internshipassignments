886e87e4932a9e472e8819cbb487eca3808d6276
Amirtha Krishnan <76719897+ArceusMaxis@users.noreply.github.com>
2024-03-26 15:02:05 +0000

    better layout fuel tracker

--- pages/Fuel_Price_Tracker.py
+++ pages/Fuel_Price_Tracker.py
@@ -2,7 +2,7 @@ import streamlit as st
 import requests
 from bs4 import BeautifulSoup
 
-def fetch_petrol_price():
+def fetch_tn_petrol_price():
     url = "https://www.businesstoday.in/fuel-price/petrol-price-in-chennai-today"
     response = requests.get(url)
     soup = BeautifulSoup(response.content, 'html.parser')
@@ -12,7 +12,7 @@ def fetch_petrol_price():
     else:
         return 
     
-def fetch_diesel_price():
+def fetch_tn_diesel_price():
     url = "https://www.businesstoday.in/fuel-price/diesel-price-in-chennai-today"
     response = requests.get(url)
     soup = BeautifulSoup(response.content, 'html.parser')
@@ -22,21 +22,70 @@ def fetch_diesel_price():
     else:
         return None
 
+def fetch_py_petrol_price():
+    url = "https://www.businesstoday.in/fuel-price/petrol-price-in-pondicherry-today"
+    response = requests.get(url)
+    soup = BeautifulSoup(response.content, 'html.parser')
+    price_element = soup.find(id="render_today_price")
+    if price_element:
+        return price_element.text.strip()
+    else:
+        return 
+    
+def fetch_py_diesel_price():
+    url = "https://www.businesstoday.in/fuel-price/diesel-price-in-pondicherry-today"
+    response = requests.get(url)
+    soup = BeautifulSoup(response.content, 'html.parser')
+    price_element = soup.find(id="render_today_price")
+    if price_element:
+        return price_element.text.strip()
+    else:
+        return None
+
 def main():
-    st.title("Tamil Nadu Fuel Prices Today")
+    tn,py = st.columns(2)
 
-    # Fetch petrol price
-    petrol_price = fetch_petrol_price()
-    diesel_price = fetch_diesel_price()
+    st.title("Fuel Price Tracker")
+    st.markdown("""
+    <style>
+    .big-font {
+        font-size:300px !important;
+    }
+    </style>
+    """, unsafe_allow_html=True)
 
-    # Display price
-    if petrol_price:
-        st.write(f"Today's Petrol Price in Chennai: {petrol_price}")
+    tn_petrol_price = fetch_tn_petrol_price()
+    tn_diesel_price = fetch_tn_diesel_price()
+    py_petrol_price = fetch_py_petrol_price()
+    py_diesel_price = fetch_py_diesel_price()
+    
+    with tn:
+        st.header("Tamil Nadu Prices",divider='green')
+        st.write(f"Petrol Price:")
+        tn_pp = st.empty()
+        tn_pp.write(f"{tn_petrol_price}")
+        st.divider()
+        st.write(f"Diesel Price:")
+        tn_pp = st.empty()
+        tn_pp.write(f"{tn_diesel_price}")
+
+    with py:
+        st.header("Puducherry Prices",divider='green')
+        st.write(f"Petrol Price:")
+        tn_pp = st.empty()
+        tn_pp.write(f"{py_petrol_price}")
+        st.divider()
+        st.write(f"Diesel Price:")
+        tn_pp = st.empty()
+        tn_pp.write(f"{py_diesel_price}")
+
+    if tn_petrol_price:
+        st.write(f"Today's Petrol Price in Chennai: {tn_petrol_price}")
     else:
         st.error("Failed to fetch petrol price. Please try again later.")
     
-    if diesel_price:
-        st.write(f"Today's Diesel Price in Chennai: {diesel_price}")
+    if tn_diesel_price:
+        st.write(f"Today's Diesel Price in Chennai: {tn_diesel_price}")
     else:
         st.error("Failed to fetch petrol price. Please try again later.")
 
