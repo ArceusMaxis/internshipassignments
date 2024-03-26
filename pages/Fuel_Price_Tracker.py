1e19ea6c58f03fd615fd8473f74bc67b96b3c032
Amirtha Krishnan <76719897+ArceusMaxis@users.noreply.github.com>
2024-03-26 14:45:24 +0000

    diesel price integrated too

--- pages/Fuel_Price_Tracker.py
+++ pages/Fuel_Price_Tracker.py
@@ -10,17 +10,33 @@ def fetch_petrol_price():
     if price_element:
         return price_element.text.strip()
     else:
+        return 
+    
+def fetch_diesel_price():
+    url = "https://www.businesstoday.in/fuel-price/diesel-price-in-chennai-today"
+    response = requests.get(url)
+    soup = BeautifulSoup(response.content, 'html.parser')
+    price_element = soup.find(id="render_today_price")
+    if price_element:
+        return price_element.text.strip()
+    else:
         return None
 
 def main():
-    st.title("Chennai Petrol Price Today")
+    st.title("Tamil Nadu Fuel Prices Today")
 
     # Fetch petrol price
     petrol_price = fetch_petrol_price()
+    diesel_price = fetch_diesel_price()
 
     # Display price
     if petrol_price:
         st.write(f"Today's Petrol Price in Chennai: {petrol_price}")
+    else:
+        st.error("Failed to fetch petrol price. Please try again later.")
+    
+    if diesel_price:
+        st.write(f"Today's Petrol Price in Chennai: {diesel_price}")
     else:
         st.error("Failed to fetch petrol price. Please try again later.")
 
